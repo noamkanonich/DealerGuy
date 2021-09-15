d92f9f0c04d92c53b20c4a424e18895d5e4d284a
Noam Kanonich <55742997+noamkanonich@users.noreply.github.com>
2021-09-15 18:59:38 +0300

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -29,8 +29,8 @@ function hideDiv() {
 
     const items = document.querySelectorAll(".accordion a");
     function toggleAccordion(){
-    this.classList.toggle('active');
-    this.nextElementSibling.classList.toggle('active');
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('active');
     }
     
     items.forEach(item => item.addEventListener('click', toggleAccordion));
